768c6d0188cd9f7027f8d4df0dd3c81b68faaf9f
filipfuks2005@gmail.com <filipfuks@Filips-Mac-mini.local>
2026-06-12 15:24:32 +0200

    chore: init project strucutre

---
+++ src/test/java/com/example/Otomoto_clone/OtomotoCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Otomoto_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtomotoCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
